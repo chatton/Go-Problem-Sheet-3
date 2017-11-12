402c5916d84cc3c0030ad6f09f89280bf04013a1
chatton <cianhatton@gmail.com>
2017-11-12 00:21:25 +0000

    added solution to problem 5

--- src/main.go
+++ src/main.go
@@ -1,45 +1,78 @@
 package main
 
 import (
-	"math/rand"
 	"fmt"
-	"time"
+	"math/rand"
 	"os"
 	"regexp"
+	"strings"
+	"time"
 )
 
-func ElizaResponse (input string) string {
+func applyReflections(original string) string {
+	// reflection map adapted from https://www.smallsurething.com/implementing-the-famous-eliza-chatbot-in-python/
 
-	pattern := regexp.MustCompile(`\b(?i)father`) // (?i) makes pattern lower case, \b to indicate word end.
-	if pattern.MatchString(input) {
-		return "Why don’t you tell me more about your father?"
-	} else { // doesn't contain father
-		iAmRe := regexp.MustCompile("(?i)^(i am|im|i'm) (.*)")
-		if iAmRe.MatchString(input) { // capture groups are ["fullMatch", "I'm", "topic"]
-			captured := iAmRe.FindStringSubmatch(input)[2] // 2 is what was after the *I'm* variant.
-			return fmt.Sprintf("How do you know you are %s?", captured)
+	reflections := map[string]string{
+		"am":     "are",
+		"was":    "were",
+		"i":      "you",
+		"i'd":    "you would",
+		"i've":   "you have",
+		"i'll":   "you will",
+		"my":     "your",
+		"are":    "am",
+		"you've": "I have",
+		"you'll": "I will",
+		"your":   "my",
+		"yours":  "mine",
+		"you":    "I",
+		"me":     "you",
+	}
+
+	words := strings.Split(original, " ") // break the string into words
+
+	for index, word := range words {
+		// if the word is in the map, it needs to be changed
+		if val, ok := reflections[strings.ToLower(word)]; ok {
+			words[index] = val // update the current index to be the mapped word
 		}
 	}
 
+	return strings.Join(words, " ") // re-construct the string with the edited words
+}
+
+func ElizaResponse(input string) string {
+	pattern := regexp.MustCompile(`\b(?i)father`) // (?i) makes pattern lower case, \b to indicate word end.
+	if pattern.MatchString(input) {
+		return "Why don’t you tell me more about your father?"
+	}
+	// doesn't contain father
+	iAmRe := regexp.MustCompile(`(?i)^(i am|im|i'm) (.*)[\.?!]`) // requires that the input end with some form of sentence terminator.
+	if iAmRe.MatchString(input) {                                // capture groups are ["fullMatch", "I'm", "topic"]
+		captured := iAmRe.FindStringSubmatch(input)[2] // 2 is what was after the *I'm* variant.
+		captured = applyReflections(captured)
+		return fmt.Sprintf("How do you know you are %s?", captured)
+	}
+
 	possibleResponses := []string{
-		"I’m not sure what you’re trying to say. Could you explain it to me?", 
-		"How does that make you feel?", 
+		"I’m not sure what you’re trying to say. Could you explain it to me?",
+		"How does that make you feel?",
 		"Why do you say that?"}
-	
+
 	numResponses := len(possibleResponses) // total number of responses
-	index := rand.Intn(numResponses) // index between 0 -> len - 1
-	return possibleResponses[index] // choose the random element.
+	index := rand.Intn(numResponses)       // index between 0 -> len - 1
+	return possibleResponses[index]        // choose the random element.
 }
 
 func printQuestionAndResponse(input string) {
 	fmt.Fprintf(os.Stdout, "Input: %s - Response: %s\n", input, ElizaResponse(input))
 }
 
-func main(){
+func main() {
 	rand.Seed(time.Now().UnixNano()) // ensure we don't get the same values each time
-	
+
 	inputs := []string{
-		"Im happy",
+		"Im happy.",
 		"I'm not happy with your responses.",
 		"im not sure that you understand the effect that your questions are having on me.",
 		"I am supposed to just take what you’re saying at face value?"}
